710bf0828cdaa830e98410c7ce2cfe435bedaf76
Diego Pessoa <dabpessoa@gmail.com>
2017-01-30 12:01:11 -0300

    Início de testes da tela UI.

---
+++ src/com/atma/superpomo/gui/controller/PrincipalUIController.java
@@ -0,0 +1,16 @@
+package com.atma.superpomo.gui.controller;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Created by diego.pessoa on 30/01/2017.
+ */
+public class PrincipalUIController extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+
+}
---
+++ src/com/atma/superpomo/gui/principalUI.java
@@ -0,0 +1,7 @@
+package com.atma.superpomo.gui;
+
+/**
+ * Created by diego.pessoa on 30/01/2017.
+ */
+public class PrincipalUI {
+}
