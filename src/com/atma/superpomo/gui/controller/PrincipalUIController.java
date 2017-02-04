57c6e824f03b6a7f5ea520d07ae6311fd73682d1
dabpessoa <dabpessoa@gmail.com>
2017-02-04 17:55:46 -0300

    .

--- src/com/atma/superpomo/gui/controller/PrincipalUIController.java
+++ src/com/atma/superpomo/gui/controller/PrincipalUIController.java
@@ -1,26 +1,17 @@
 package com.atma.superpomo.gui.controller;
 
-import com.atma.superpomo.gui.PrincipalUI;
 import com.atma.superpomo.model.Pomodoro;
-import com.atma.superpomo.service.tasks.MinutePomoTask;
 import com.atma.superpomo.service.tasks.SecondPomoTask;
 import com.atma.superpomo.service.timer.PomoTimerTask;
 import com.atma.superpomo.service.timer.PomoTimerTaskListener;
-import javafx.application.Application;
 import javafx.application.Platform;
-import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.stage.Stage;
 
-import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.ResourceBundle;
-import java.util.concurrent.ExecutorService;
 
 /**
  * Created by diego.pessoa on 30/01/2017.
